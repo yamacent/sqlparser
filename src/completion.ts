1ce2211557706917875ba4ef8b25ae2a75ce1a8d
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2021-01-26 22:57:35 +0900

    Add note

--- src/completion.ts
+++ src/completion.ts
@@ -6,3 +6,72 @@ export function complete(code: string, pos: Position) {
   const nodes = parser.parse()
   console.log(nodes)
 }
+
+/*
+Tables
+  - emp (id, first, last, deptId, role)
+  - dept (id, name)
+
+select | from emp
+=> id, first, last, deptId, role
+
+{
+  type: Prog,
+  pos,
+  nodes: [
+    {
+      type: Select,
+      pos,
+      nodes: []
+    },
+    {
+      type: From,
+      pos,
+      nodes: [
+        {
+          type: Ident,
+          pos,
+          val: emp
+        }
+      ]
+    }
+  ]
+}
+
+select from |
+=> emp, dept
+
+select | from emp e join dept d on e.deptId = d.id
+=> id, first, last, deptId, role, id, name
+
+select d.| from emp e join dept d on e.deptId = d.id
+=> id, name
+
+select | from (select 123 as foo, 456 as bar)
+=> foo, bar
+
+select | from (select * from dept)
+=> id, name
+
+select | from (select id from dept)
+=> id
+
+with w1 as (
+  select * from |
+)
+=> emp, dept
+
+with w1 as (
+  select 'foo' as foo
+)
+select * from |
+=> w1, emp, dept
+
+with w1 as (
+  select 'foo' as foo
+)
+select | from w1
+=> foo
+
+codntion, case, union, expression, function call, comparison
+*/
